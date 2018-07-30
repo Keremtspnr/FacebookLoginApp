36f79ea05a91a1cc0aa6d50aa8553f70d034e486
Kerem Taşpınar <keremtaspinar@gmail.com>
2018-07-30 22:38:55 +0300

    Add files via upload

---
+++ assets/layouts/footer.php
@@ -0,0 +1,6 @@
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ assets/layouts/header.php
@@ -0,0 +1,44 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="tr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Facebook Login for Gezer</title>
+  </head>
+  <body>
+    <?php require_once 'src/Facebook/autoload.php'; // change path as needed ?>
+    <?php
+    $fb = new \Facebook\Facebook([
+      'app_id' => '262413657681286',
+      'app_secret' => '75929c699f2b1509c5a4ea5e6144b156',
+      'default_graph_version' => 'v3.1',
+      //'default_access_token' => '{access-token}', // optional
+    ]);
+    ?>
+        <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Facebook Panel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">Anasayfa</a>
+            </li>
+            <li class="nav-item">
+              <?php if($_SESSION['login']==true && isset($_SESSION['fb_access_token'])){?>
+                <a class="nav-link" href="logout.php">Çıkışa</a>
+              <?php } else { ?>
+                <a class="nav-link" href="#">Girişz</a>
+              <?php } ?>
+              <a class="nav-link" href="#">Girişz</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
